dc04f48fc9e8b5fda63901f02edc9fe9c76d49ec
Lucas Silva <lucas.silva98@outlook.pt>
2024-01-31 14:53:06 +0000

    google maps connection

--- testeModelo1/src/screens/Stations.jsx
+++ testeModelo1/src/screens/Stations.jsx
@@ -1,16 +1,35 @@
 // StationsScreen.js
 
 import React, {useState, useEffect} from 'react';
-import {View, StyleSheet} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  ActivityIndicator,
+  PermissionsAndroid,
+} from 'react-native';
 import MapView, {Marker} from 'react-native-maps';
 import axios from 'axios';
 
-const busStopIcon = require('../assets/bus-stop.png');
-
 const StationsScreen = () => {
   const [stations, setStations] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    const requestLocationPermission = async () => {
+      try {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        );
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          loadStations();
+        } else {
+          console.error('Location permission denied');
+        }
+      } catch (error) {
+        console.error('Error requesting location permission:', error);
+      }
+    };
+
     const loadStations = async () => {
       try {
         const response = await axios.get(
@@ -19,37 +38,55 @@ const StationsScreen = () => {
         setStations(response.data);
       } catch (error) {
         console.error('Error loading stations:', error);
+      } finally {
+        setLoading(false);
       }
     };
 
-    loadStations();
+    requestLocationPermission();
   }, []);
 
   return (
     <View style={{flex: 1}}>
-      <MapView
-        style={{flex: 1}}
-        initialRegion={{
-          latitude: 41.354639,
-          longitude: -8.756689,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}>
-        {stations.map(station => (
-          <Marker
-            key={station.id} // Ensure each marker has a unique key
-            coordinate={{
-              latitude: station.latitude,
-              longitude: station.longitude,
-            }}
-            title={station.name}
-            description={station.description}
-            image={busStopIcon}
-          />
-        ))}
-      </MapView>
+      {loading ? (
+        <ActivityIndicator style={styles.loader} size="large" />
+      ) : (
+        <MapView
+          style={{flex: 1}}
+          initialRegion={{
+            latitude: 41.354639,
+            longitude: -8.756689,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}>
+          {stations.map(station =>
+            // Check if station.id is defined before rendering Marker
+            // If not defined, you can use an alternative unique identifier or skip rendering
+            station.id ? (
+              <Marker
+                key={station.id}
+                coordinate={{
+                  latitude: station.location.latitude,
+                  longitude: station.location.longitude,
+                }}
+                title={station.name}
+                description={station.description}
+                pinColor={'blue'} // Customize the marker color
+              />
+            ) : null,
+          )}
+        </MapView>
+      )}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  loader: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default StationsScreen;
--- testeModelo2/src/screens/DetailsScreen.jsx
+++ testeModelo2/src/screens/DetailsScreen.jsx
@@ -1,7 +1,7 @@
 // Importações necessárias
 import React from 'react';
-import {View, Text, Image} from 'react-native';
-import MapView, {Marker} from 'react-native-maps';
+import {View, Text, Image, StyleSheet} from 'react-native';
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 
 const DetailsScreen = ({route}) => {
   // Obter dados do anúncio da rota
@@ -20,23 +20,32 @@ const DetailsScreen = ({route}) => {
 
       {/* Mapa com a localização do anúncio */}
       <MapView
-        style={{width: '100%', height: 300}}
+        provider={PROVIDER_GOOGLE}
+        style={styles.maps}
         initialRegion={{
           latitude: ad.location.latitude,
           longitude: ad.location.longitude,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
+          latitudeDelta: 0.05,
+          longitudeDelta: 0.05,
         }}>
         <Marker
           coordinate={{
             latitude: ad.location.latitude,
             longitude: ad.location.longitude,
           }}
-          title="Localização do Anúncio"
+          title={ad.brand}
+          description={ad.model}
         />
       </MapView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  maps: {
+    width: '100%',
+    height: 300,
+  },
+});
 
 export default DetailsScreen;
